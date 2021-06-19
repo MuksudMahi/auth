9635d64c8ea12af22c7e5ebb66805877dfeda695
Muksud Mahi <mmahi1@my.centennialcollege.ca>
2021-06-18 23:32:18 -0400

    Add mongodb atlas uri

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/portfolio"
+    //"URI": "mongodb://localhost/portfolio"
+    "URI": "mongodb+srv://MuksudMahi:t4U4Nb4XAzrr8kgT@mongodbserver.jr4p4.mongodb.net/portfolio?retryWrites=true&w=majority"
 }
